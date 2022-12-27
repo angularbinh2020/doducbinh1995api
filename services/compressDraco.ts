a1d56eb7a2c6c0eb6275e22cb4764f294f9131e9
angularbinh2020 <61402063+angularbinh2020@users.noreply.github.com>
2022-12-27 14:53:41 +0700

    update

--- services/compressDraco.ts
+++ services/compressDraco.ts
@@ -5,6 +5,7 @@ import axios from "axios";
 import { Mesh } from "three";
 import { OBJLoader } from "./OBJLoader";
 import logger from "./logger";
+import decompress from "decompress";
 
 let encoderModule!: draco3d.EncoderModule;
 
@@ -33,133 +34,139 @@ export async function CompressDraco() {
   //       method: "GET",
   //     }
   //   );
-  const fileZip = fs.readFileSync(
-    __dirname + "/221222-225101_OBJ-ba16 (1).zip"
-  );
-  const zip = await JSZip.loadAsync(fileZip);
-  const isLoadSuccess = response.status === 200 || response.status === 0;
-  if (isLoadSuccess) {
-    const zip = await JSZip.loadAsync(fileZip);
-    let mtlFilePath = "";
-    let objFile!: JSZip.JSZipObject;
-    let mtlFile!: JSZip.JSZipObject;
-    let textureFile!: JSZip.JSZipObject;
-    let plyFile!: JSZip.JSZipObject;
-    zip.forEach(function (relativePath, file) {
-      if (relativePath.endsWith(".obj")) objFile = file;
-      if (relativePath.endsWith(".mtl")) {
-        mtlFilePath = relativePath;
-        mtlFile = file;
-      }
-      if (relativePath.endsWith(".jpg")) textureFile = file;
-      if (relativePath.endsWith(".ply")) plyFile = file;
+  //   const fileZip = fs.readFileSync(
+  //     __dirname + "/221222-225101_OBJ-ba16 (1).zip"
+  //   );
+  //   const zip = await JSZip.loadAsync(fileZip);
+  //   const isLoadSuccess = response.status === 200 || response.status === 0;
+  //   if (isLoadSuccess) {
+  //     const zip = await JSZip.loadAsync(fileZip);
+  //     let mtlFilePath = "";
+  //     let objFile!: JSZip.JSZipObject;
+  //     let mtlFile!: JSZip.JSZipObject;
+  //     let textureFile!: JSZip.JSZipObject;
+  //     let plyFile!: JSZip.JSZipObject;
+  //     zip.forEach(function (relativePath, file) {
+  //       if (relativePath.endsWith(".obj")) objFile = file;
+  //       if (relativePath.endsWith(".mtl")) {
+  //         mtlFilePath = relativePath;
+  //         mtlFile = file;
+  //       }
+  //       if (relativePath.endsWith(".jpg")) textureFile = file;
+  //       if (relativePath.endsWith(".ply")) plyFile = file;
+  //     });
+  //     if (plyFile) {
+  //     } else {
+  //       const objContent = await objFile.async("blob");
+  //       //@ts-ignore
+  //       const group = await new OBJLoader().loadAsync(
+  //         URL.createObjectURL(objContent)
+  //       );
+  //       let indices: any[] = [];
+  //       let vertices: any = [];
+  //       let normals: any = [];
+  //       group.traverse((object: any) => {
+  //         if (object instanceof Mesh && object.geometry) {
+  //           const attributes = object.geometry.attributes;
+  //           attributes.normal.array.forEach(function (n: any) {
+  //             normals.push(n);
+  //           });
+  //           attributes.uv.array.forEach(function (u: any) {
+  //             indices.push(u);
+  //           });
+  //           attributes.position.array.forEach(function (p: any) {
+  //             vertices.push(p);
+  //           });
+  //         }
+  //       });
+  //       const mesh: any = {
+  //         indices: new Uint32Array(indices),
+  //         vertices: new Float32Array(vertices),
+  //         normals: new Float32Array(normals),
+  //       };
+  //       const dracoMesh = new encoderModule.Mesh();
+
+  //       const numFaces = mesh.indices.length / 3;
+  //       const numPoints = mesh.vertices.length;
+  //       meshBuilder.AddFacesToMesh(dracoMesh, numFaces, mesh.indices);
+
+  //       meshBuilder.AddFloatAttribute(
+  //         dracoMesh,
+  //         encoderModule.POSITION,
+  //         numPoints,
+  //         3,
+  //         mesh.vertices
+  //       );
+  //       if (mesh.hasOwnProperty("normals")) {
+  //         meshBuilder.AddFloatAttribute(
+  //           dracoMesh,
+  //           encoderModule.NORMAL,
+  //           numPoints,
+  //           3,
+  //           mesh.normals
+  //         );
+  //       }
+  //       if (mesh.hasOwnProperty("colors")) {
+  //         meshBuilder.AddFloatAttribute(
+  //           dracoMesh,
+  //           encoderModule.COLOR,
+  //           numPoints,
+  //           3,
+  //           mesh.colors
+  //         );
+  //       }
+  //       if (mesh.hasOwnProperty("texcoords")) {
+  //         meshBuilder.AddFloatAttribute(
+  //           dracoMesh,
+  //           encoderModule.TEX_COORD,
+  //           numPoints,
+  //           3,
+  //           mesh.texcoords
+  //         );
+  //       }
+
+  //       //   if (method === "edgebreaker") {
+  //       //     encoder.SetEncodingMethod(encoderModule.MESH_EDGEBREAKER_ENCODING);
+  //       //   } else if (method === "sequential") {
+  //       //     encoder.SetEncodingMethod(encoderModule.MESH_SEQUENTIAL_ENCODING);
+  //       //   }
+
+  //       const encodedData = new encoderModule.DracoInt8Array();
+  //       // Use default encoding setting.
+  //       const encodedLen = encoder.EncodeMeshToDracoBuffer(
+  //         dracoMesh,
+  //         encodedData
+  //       );
+  //       const outputBuffer = new ArrayBuffer(encodedLen);
+  //       const outputData = new Int8Array(outputBuffer);
+  //       for (let i = 0; i < encodedLen; ++i) {
+  //         outputData[i] = encodedData.GetValue(i);
+  //       }
+  //       fs.writeFile(
+  //         "bunny_10.drc",
+  //         Buffer.from(outputBuffer),
+  //         "binary",
+  //         function (err) {
+  //           if (err) {
+  //             console.log(err);
+  //           } else {
+  //             console.log("The file was saved!");
+  //           }
+  //         }
+  //       );
+  //       encoderModule.destroy(encodedData);
+  //       encoderModule.destroy(dracoMesh);
+  //       encoderModule.destroy(encoder);
+  //       encoderModule.destroy(meshBuilder);
+  //       logger.info("Compress success");
+  //     }
+  //   } else {
+  //     console.log("response status: " + response.statusText);
+  //   }
+
+  decompress(__dirname + "/221222-225101_OBJ-ba16 (1).zip").then((files) => {
+    files.forEach((file) => {
+      logger.info(file.path);
     });
-    if (plyFile) {
-    } else {
-      const objContent = await objFile.async("blob");
-      //@ts-ignore
-      const group = await new OBJLoader().loadAsync(
-        URL.createObjectURL(objContent)
-      );
-      let indices: any[] = [];
-      let vertices: any = [];
-      let normals: any = [];
-      group.traverse((object: any) => {
-        if (object instanceof Mesh && object.geometry) {
-          const attributes = object.geometry.attributes;
-          attributes.normal.array.forEach(function (n: any) {
-            normals.push(n);
-          });
-          attributes.uv.array.forEach(function (u: any) {
-            indices.push(u);
-          });
-          attributes.position.array.forEach(function (p: any) {
-            vertices.push(p);
-          });
-        }
-      });
-      const mesh: any = {
-        indices: new Uint32Array(indices),
-        vertices: new Float32Array(vertices),
-        normals: new Float32Array(normals),
-      };
-      const dracoMesh = new encoderModule.Mesh();
-
-      const numFaces = mesh.indices.length / 3;
-      const numPoints = mesh.vertices.length;
-      meshBuilder.AddFacesToMesh(dracoMesh, numFaces, mesh.indices);
-
-      meshBuilder.AddFloatAttribute(
-        dracoMesh,
-        encoderModule.POSITION,
-        numPoints,
-        3,
-        mesh.vertices
-      );
-      if (mesh.hasOwnProperty("normals")) {
-        meshBuilder.AddFloatAttribute(
-          dracoMesh,
-          encoderModule.NORMAL,
-          numPoints,
-          3,
-          mesh.normals
-        );
-      }
-      if (mesh.hasOwnProperty("colors")) {
-        meshBuilder.AddFloatAttribute(
-          dracoMesh,
-          encoderModule.COLOR,
-          numPoints,
-          3,
-          mesh.colors
-        );
-      }
-      if (mesh.hasOwnProperty("texcoords")) {
-        meshBuilder.AddFloatAttribute(
-          dracoMesh,
-          encoderModule.TEX_COORD,
-          numPoints,
-          3,
-          mesh.texcoords
-        );
-      }
-
-      //   if (method === "edgebreaker") {
-      //     encoder.SetEncodingMethod(encoderModule.MESH_EDGEBREAKER_ENCODING);
-      //   } else if (method === "sequential") {
-      //     encoder.SetEncodingMethod(encoderModule.MESH_SEQUENTIAL_ENCODING);
-      //   }
-
-      const encodedData = new encoderModule.DracoInt8Array();
-      // Use default encoding setting.
-      const encodedLen = encoder.EncodeMeshToDracoBuffer(
-        dracoMesh,
-        encodedData
-      );
-      const outputBuffer = new ArrayBuffer(encodedLen);
-      const outputData = new Int8Array(outputBuffer);
-      for (let i = 0; i < encodedLen; ++i) {
-        outputData[i] = encodedData.GetValue(i);
-      }
-      fs.writeFile(
-        "bunny_10.drc",
-        Buffer.from(outputBuffer),
-        "binary",
-        function (err) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("The file was saved!");
-          }
-        }
-      );
-      encoderModule.destroy(encodedData);
-      encoderModule.destroy(dracoMesh);
-      encoderModule.destroy(encoder);
-      encoderModule.destroy(meshBuilder);
-      logger.info("Compress success");
-    }
-  } else {
-    console.log("response status: " + response.statusText);
-  }
+  });
 }
